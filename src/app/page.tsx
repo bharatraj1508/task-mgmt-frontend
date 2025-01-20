1e6a3a8961792f2cec256ed77322271d799eb11e
Bharat Raj Verma <bharat.raj1508@gmail.com>
2025-01-20 15:25:05 +0530

    created the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-import Image from "next/image";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return <div>Root</div>;
+  const router = useRouter();
+  return (
+    <div className="flex items-center justify-center gap-8 mt-16">
+      <Button onClick={() => router.push("/login")}>Login</Button>
+      <Button onClick={() => router.push("/signup")}>Get Started</Button>
+    </div>
+  );
 }
